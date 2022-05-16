7c6c391e85bd0268972e26280a4ec205f981787b
Deepika-05-Binjola <102277641+Deepika-05-Binjola@users.noreply.github.com>
2022-05-16 13:56:27 +0530

    Add files via upload

---
+++ ques1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter the value of a= "<<endl;
+cin>>a;
+cout<<"enter the value of b= "<<endl;
+cin>>b;
+cout<<"quotient of a/b= "<<a/b<<endl;
+cout<<"remainder of a/b= "<<a%b;
+return 0;
+
+
+}
